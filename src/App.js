47d4dc85041ca17df3c685ba9c345cff042824da
Alex Pletis <alexanderpletsis@gmail.com>
2019-04-29 16:15:11 +0300

    css2scss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Main from './Main';
-import './App.css';
+import './App.scss';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 class App extends Component {
